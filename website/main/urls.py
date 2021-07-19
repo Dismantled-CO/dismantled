a19c3c73ecbab6e445cacd51a90fafc0de371963
Aditya Medhe <medhe.aditya@gmail.com>
2021-07-19 15:24:32 +0530

    register and login
    register and login features created and are mostly working

--- website/main/urls.py
+++ website/main/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.indexPage, name='index'),
+    path('login', views.loginPage, name='login'),
+    path('register', views.registerPage, name='register')
 ]
--- website/main/views.py
+++ website/main/views.py
@@ -1,10 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
+from .forms import CreateUserForm
 
-def index(request):
-    template = loader.get_template('main/templates/index.html')
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+def indexPage(request):
+    template = loader.get_template('main/index.html')
     context = {}
+    return HttpResponse(template.render(context, request))
+
+def loginPage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, password=password, username=username)
+
+        if user is not None:
+            login(request, user)
+            redirect('index')
+
+    template = loader.get_template('main/login.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            message.success(request, 'Account was successfully created for ' +  user)
+            return redirect('login')
+
+    template = loader.get_template('main/register.html')
+    context = {'form': form}
     return HttpResponse(template.render(context, request))
